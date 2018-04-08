cb1bde73b435f9780eb8bd80651bd14b0d1288c9
LaQuita <lgollman@wayne.edu>
2018-04-08 00:20:01 -0700

    version 3 using async/await
    uses async/await instead of promises

---
+++ easyHTTP3/easyhttp3.js
@@ -0,0 +1,56 @@
+/**
+ * EasyHTTP Library
+ * Library for making HTTP requests
+ *
+ * @version 3.0.0
+ * @author LaQuita G
+ * @license MIT
+ *
+ * **/
+
+class EasyHTTP {
+  // Make an HTTP GET request
+  // async get(url) {
+  //   const response = await fetch(url);
+  //   const resData = await response.json();
+  //   return resData;
+  // }
+
+  //Make an HTTP POST Request
+  // async post(url, data) {
+  //   const response = await fetch(url, {
+  //     method: 'POST',
+  //     headers: {
+  //       'Content-type': 'application/json'
+  //     },
+  //     body: JSON.stringify(data)
+  //   });
+  //   const resData = await response.json();
+  //   return resData;
+  // }
+
+  //Make an HTTP PUT request
+  // async put(url, data) {
+  //   const response = await fetch(url, {
+  //     method: 'PUT',
+  //     headers: {
+  //       'Content-type': 'application/json'
+  //     },
+  //     body: JSON.stringify(data)
+  //   });
+  //   const resData = await response.json();
+  //   return resData;
+  // }
+
+  //Make an HTTP DELETE request
+  async delete(url) {
+    const response = await fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json'
+      }
+    });
+    const resData = await 'Resource deleted';
+    return resData;
+  }
+}
